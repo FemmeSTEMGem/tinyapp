8782a06ca03b79f357b589361adf1f4c3c5b3bbd
FemmeSTEMGem <femmestemgem@gmail.com>
2022-01-25 17:32:31 -0500

    redirected short URLs

--- express_server.js
+++ express_server.js
@@ -44,14 +44,19 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  console.log(req.params)
+  console.log("req.params: ", req.params)
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
 });
 
 
 //POSTS
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  console.log("req.body: ", req.body);  // Log the POST request body to the console
   
   const shortURL = generateRandomString()
   urlDatabase[shortURL] = req.body.longURL
